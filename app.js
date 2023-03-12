c8f57d1bec3abd468b235dbf20ffdb4e57fab8dd
SIGMAGHOST <abdullahsajid1098@gmail.com>
2023-03-12 15:00:35 +0500

    Modal Opener

---
+++ app.js
@@ -0,0 +1,34 @@
+var modalButton = document.querySelectorAll('.modalButton');
+var overlay = document.querySelector('.overlay');
+var modal = document.querySelector('.modal');
+var closeBtn = document.querySelector('.close');
+const body = document.querySelector('body');
+
+for (i = 0; i < modalButton.length; i++) {
+
+    modalButton[i].addEventListener('click', removeHidden);
+}
+
+overlay.addEventListener('click', addHidden)
+closeBtn.addEventListener('click', addHidden)
+body.addEventListener('keydown', ecapeKey);
+
+function addHidden() {
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    // overlay.classList.remove('transitionIn');
+    // modal.classList.remove('transitionIn');
+}
+function removeHidden() {
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    // overlay.classList.add('transitionIn');
+    // modal.classList.add('transitionIn');
+    // console.log(overlay.classList)
+}
+function ecapeKey(a) {
+    if (a.key === "Escape") {
+        overlay.classList.add('hidden');
+        modal.classList.add('hidden');
+    }
+}
